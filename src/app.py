751874512c28ba6628191368f9e3ebf6c798f70f
-TΦM- <137113975+JTomas88@users.noreply.github.com>
2024-07-19 11:32:23 +0000

    Terminado

--- src/app.py
+++ src/app.py
@@ -5,17 +5,20 @@ import os
 from flask import Flask, request, jsonify, url_for
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from datastructures import FamilyStructure
+from datastructures import FamilyStructure #La importación se hace al revés. Se importa "desde...el archivo x"
 #from models import Person
 
+#Configuración de Flask que nos configura el servidor. 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 CORS(app)
 
-# create the jackson family object
+# Creamos una instancia de la estructura que tiene datastructures.py.
+# Concretamente llama a la class FamilyStructure, que tiene el init para que la clase pueda crearse. 
+# Le pasamos "Jackson" porque es el valor que recibe la clase en last_name. 
 jackson_family = FamilyStructure("Jackson")
 
-# Handle/serialize errors like a JSON object
+# Permite que los errores se vean como un objeto Json
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
@@ -25,18 +28,54 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/members', methods=['GET'])
-def handle_hello():
+# Inicialización de la ruta /members
+# Dentro crea la función que crea una variable "members" que recibe lo que se obtiene de get_all_members (devuelve members
+# que es un array) Luego en el response devuelve la lista de members 
 
-    # this is how you can use the Family datastructure by calling its methods
+@app.route('/members', methods=['GET'])
+def handle_hello():    
     members = jackson_family.get_all_members()
     response_body = {
         "hello": "world",
         "family": members
     }
-
-
     return jsonify(response_body), 200
+
+
+
+# Creamos la ruta en la api y el servicio que va a recibir los datos
+# necesarios para crear un nuevo miembro. Le quitamos el dato id, porque
+# lo vamos a generar nosotros (en la función de _generateId. )
+# Guardamos los datos recibidos en new_member
+@app.route('/members', methods=['POST'])
+def add_member():
+    new_member = request.json
+    member = jackson_family.add_member(new_member)
+    if (member):
+        return jsonify ({'Mensaje' : 'nuevo miembro añadido'}), 200
+    return jsonify ({'Error' : 'error al añadir'}), 400
+
+
+# ------- ENDPOINT PARA BORRAR A UN MIEMBRO ---------
+@app.route ('/member/<int:member_id>', methods = ['DELETE'])
+def delete_member(member_id):
+    delete_member = request.json #???????????????????
+    miembro_borrado = jackson_family.delete_member(member_id)
+    if (miembro_borrado):
+        return jsonify ({'Mensaje' : 'miembro borrado'}), 200
+    return jsonify ({'Error' : 'error al borrar miembro'}), 400
+
+
+# ------- ENDPOINT PARA MOSTRAR A UN MIEMBRO ---------
+@app.route ('/mostrar/<int:member_id>', methods = ['GET'])
+def mostrar (member_id):
+    member =  jackson_family.get_member(member_id)
+    if (member):
+        response_body = {'Miembro' : member}
+        return (response_body), 200
+    return jsonify ({'Error' : 'el miembro indicado no existe'}), 400
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -8,29 +8,59 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+#init: es lo que va a a inicializar mi modelo (class). Siempre va a recibir self (no es una palabra reservada pero
+# se usa por convenio). Self hace referencía a sí mismo, recibe a este mismo elemento y a last_name, que será el 
+#primer elemento que reciba nuestra clase (self.last_name = last_name
+# El doble guion bajo significa que tiene una función escondida y qué solo se puede ejecutar en esta clase. 
+# Sólo se utiliza una sola vez, en el momento en el que se crea. Sirve para generar una misma estructira de familia, pero
+# pudiendo crear muchas familias a la vez. 
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
-
-        # example list of members
+        self._next_id = 1
         self._members = []
 
-    # read-only: Use this method to generate random members ID's when adding members into the list
+   
+    # Genera IDs aleatorios al crear a un nuevo miembro en la familia. 
+    # Tiene un guión bajo a diferencia del resto porque sólo vamos a poder usarlo dentro de esta clase, no fuera. 
+    # Es un método para el funcionamiento de la clase. 
     def _generateId(self):
         return randint(0, 99999999)
 
+    # MÉTODO AÑADIR MIEMBROS: Va a recibirse a sí mismo y al miembreo que se va a añadir. Se va a hacer generando el ID
+    # y añadiendo el last_name ("Jackson")
+    # member['last_name'] = self.last_name -> recibe el valor que se haya asignado al generar una familia nueva ("Jackson")
+    # member['id'] = self._generateId() -> llama al método privado que genera el id que tendrá el nuevo miembro
+    # member['lucky_numbers'] -> convierte en lista lo que le llega al acceder al atribututo l_n del miembro, si no tuviese ninguno crea con el set una lista vacía 
+    # Por ultimo añade el miembro a members con un .append y devuelve miembro. 
     def add_member(self, member):
-        # fill this method and update the return
-        pass
+        member['last_name'] = self.last_name
+        member['id'] = self._generateId()
+        member['lucky_numbers'] = list(member.get('lucky_numbers', set()))
+        self._members.append(member)
+        return member
 
     def delete_member(self, id):
-        # fill this method and update the return
-        pass
+        member_to_remove = None
+        for member in self._members:
+            if member['id'] == id:
+                member_to_remove = member
+                break        
+        if member_to_remove:
+            self._members.remove(member_to_remove)
+        
+        return self._members
+
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
-
-    # this method is done, it returns a list with all the family members
+        member_to_show = None
+        for member in self._members:
+            if member['id'] == id:
+                member_to_show = member
+                break
+        return member_to_show
+    
+  
     def get_all_members(self):
         return self._members
